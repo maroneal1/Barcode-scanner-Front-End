933859d4288cbca6b151009912fa6a39199c194b
maroneal1 <maroneal@msu.edu>
2016-11-03 20:10:59 -0400

    fuc you

--- questions/models.py
+++ questions/models.py
@@ -51,7 +51,20 @@ class LocDev(models.Model):
 class Item(models.Model):
 	item_type=models.ForeignKey( Device, on_delete=models.CASCADE)
 	item_barcode_num = models.IntegerField(default=0)
+<<<<<<< HEAD
+<<<<<<< HEAD
+	item_type = models.CharField(max_length=200)
+	user_assigned = models.CharField(max_length=200) 
+	admin = models.CharField(max_length=200) #should actually be payroll id 
+	loc_ass=models.ForeignKey( Location, on_delete=models.CASCADE)
+=======
+	user_assigned=models.CharField(max_length=200)
+	admin=models.CharField(max_length=200) #should actually be payroll id
 
+>>>>>>> 48baea82afdeef39d2c5916d77ee38f8148f88e7
+=======
+
+>>>>>>> f667fbcbb48f3c04c5842b8ede8cda1ce7f8395f
 	def get_json_object(self):
 		def access_lower_object_json(key):
 			return key.get_json_object()
@@ -68,7 +81,17 @@ class Item(models.Model):
 class Question(models.Model):
 	question_text = models.CharField(max_length=200) #field_example where is the pin?
 	pub_date = models.DateTimeField('date published') #this is obtained from ADMIN
+<<<<<<< HEAD
+<<<<<<< HEAD
+	#answers= models.ForeignKey( Choice, on_delete=models.CASCADE) #posted by users 
+	item_assoc=models.ForeignKey( Item, on_delete=models.CASCADE, null=True)
+=======
 	item_assoc=models.ForeignKey( Device, on_delete=models.CASCADE, null=True)
+>>>>>>> 48baea82afdeef39d2c5916d77ee38f8148f88e7
+	questions_loc=models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
+=======
+	item_assoc=models.ForeignKey( Device, on_delete=models.CASCADE, null=True)
+>>>>>>> f667fbcbb48f3c04c5842b8ede8cda1ce7f8395f
 	def get_json_object(self):
 		ret ={}
 		ret["question_text"]=self.question_text
@@ -78,12 +101,28 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
+<<<<<<< HEAD
+<<<<<<< HEAD
+	##question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	#above is how relationships are defined with the foreign key, each choice is related with questions 
+	choice_text = models.CharField(max_length=200)
+	scan_date_time = models.DateTimeField('datetime answered') #this is obtained from ADMIN
+	person_scanned = models.CharField(max_length=200)
+	question_associated_with= models.ForeignKey( Question, on_delete=models.CASCADE) #posted by users 
+	##votes = models.IntegerField(default=0)
+=======
+=======
+>>>>>>> f667fbcbb48f3c04c5842b8ede8cda1ce7f8395f
 	#above is how relationships are defined with the foreign key, each choice is related with questions
 	choice_text = models.CharField(max_length=200)
 	time_scanned=models.CharField(max_length=200, default= " ")
 	person_scanned=models.CharField(max_length=200, default= " ")
 	question= models.ForeignKey( Question, on_delete=models.CASCADE) #posted by users
 	location= models.ForeignKey( Location, on_delete=models.CASCADE) #posted by users
+<<<<<<< HEAD
+>>>>>>> 48baea82afdeef39d2c5916d77ee38f8148f88e7
+=======
+>>>>>>> f667fbcbb48f3c04c5842b8ede8cda1ce7f8395f
 	def __str__(self):
 		return self.choice_text
 
