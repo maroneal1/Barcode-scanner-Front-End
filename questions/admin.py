79b3427de844afa0d42a016c8940aa4f8ac8d091
maroneal1 <maroneal@msu.edu>
2016-11-03 20:16:32 -0400

    fasf

--- questions/admin.py
+++ questions/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Question,Location,Item,Choice,Device
+from .models import Question,Location,Item,Choice
 
 admin.site.register(Question)
 admin.site.register(Location)
-admin.site.register(Device)
 admin.site.register(Item)
 admin.site.register(Choice)
 # Register your models here.
